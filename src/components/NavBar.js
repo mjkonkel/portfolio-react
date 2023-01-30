7b31666ced9c282055f24ae1f1fabe41c61aeff0
mjkonkel <mitchell.konkel@gmail.com>
2023-01-29 19:13:09 -0600

    separated css files

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-
+import './NavBar.css'
 
 export default function NavBar(props) {
     return (
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,12 @@
 import resumePDF from '../assets/sample-resume.pdf'
+import './Resume.css'
 
 export default function Resume() {
 
     return (
         <div className="resume">
             <h1>Resume</h1>
-            <a href={resumePDF} target="_blank" rel="noreferrer noopener"><button>Download my resume</button></a>
+            <a href={resumePDF} target="_blank" rel="noreferrer noopener"><button>Download my Resume</button></a>
             
             <h2>My Skills</h2>
             <ul>
