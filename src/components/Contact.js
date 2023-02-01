ed552da1e2db02e9e40c266d420aaf5b9895a08d
mjkonkel <mitchell.konkel@gmail.com>
2023-01-31 18:19:20 -0600

    test deploy

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -57,7 +57,7 @@ export default function Contact() {
     return (
         <>
             <div className="contact">
-                <h1>Contact</h1>
+                <h1>Contact Me</h1>
                 <form>
                     <div>
                         <input
